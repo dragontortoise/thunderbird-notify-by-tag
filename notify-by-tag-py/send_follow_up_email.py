f8c5374af453e916c355f207058ff1c16b8adb58
dragontortoise <dragontortoise888@gmail.com>
2017-06-16 01:45:56 +0700

    It has been a long time since I last worked on this project.  I can't remember which state I was on precisely.
    Anyway, this commit has the test_grep_emails which passes.

--- notify-by-tag-py/send_follow_up_email.py
+++ notify-by-tag-py/send_follow_up_email.py
@@ -10,28 +10,52 @@ import re
 #   for each JSON
 #     send follow up emails
 
-def grep_email(input_string):
+def grep_emails(input_string):
   """
   This function greps all email addresses from the input_string and
   returns a list of them.
   """
   
-  # I got this regular expression for email from
-  # https://docs.python.org/3.1/library/re.html .
-  return re.findall('(\w+@\w+(?:\.\w+)+)', input_string)
+  # http://stackoverflow.com/questions/17681670/extract-email-sub-strin
+  # gs-from-large-document
+  return re.findall(r'[\w\.-]+@[\w\.-]+', input_string)
 
 def run():
+  """
+  myself = ['a@example.com', 'b@example.org']
 
   # A dictionary which contains:
   #   key as an email address.
   #   value as a list of email subjects.
   to_notify = {}
 
-  # load JSON data from ~/tmp/nbt-data
+  # Load JSON data from ~/tmp/nbt-data .
+  # File format of nbt-data is:
+  # 
+  #   {"subject": "<a_subject>", "author":"<an_author">,
+  #    "recipients":"<a_list_of_recipients>"}
+  #
+  # For example (without newline characters):
+  #
+  #   {"subject":"Hello World","author":"dragontortoise
+  #   <dragontortoise@example.org>","recipients":"turtle
+  #   <turtle@example.com>, duck <duck@example.com>"}
+  # 
   with open('/home/ubuntu/tmp/nbt-data') as nbt_data_file:
     for line in nbt_data_file:
       message = json.loads(line)
-      print(message['subject'])
+
+      all_email_list = grep_emails(message['author']) + \
+        grep_emails(message['recipients'])
+      subject = message['subject']
+
+      for email in all_email_list:
+        if email not in myself:
+          if email not in to_notify.keys():
+            to_notify.get[email] = [subject]
+
+  print(to_notify)
+  """
      
 if __name__ == '__main__':
   run()
--- notify-by-tag-py/test_send_follow_up_email.py
+++ notify-by-tag-py/test_send_follow_up_email.py
@@ -5,11 +5,11 @@ import unittest
 import send_follow_up_email
 
 class TestSendFollwUpEmail(unittest.TestCase):
-  def test_grep_email(self):
-    self.assertEqual(send_follow_up_email.grep_email(
-        'How are you? hello@example.com \n<world@example.local>'
+  def test_grep_emails(self):
+    self.assertEqual(send_follow_up_email.grep_emails(
+        'How are you? hello@example.com hello \n<worl.d@example.local>'
       ),
-      ['hello@example.com', 'world@example.local'])
+      ['hello@example.com', 'worl.d@example.local'])
 
 if __name__ == '__main__':
   unittest.main()
