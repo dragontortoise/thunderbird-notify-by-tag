8e52cbd68c457c55be33bb76ba276b2baff388a4
dragontortoise <dragontortoise888@gmail.com>
2017-06-16 02:28:18 +0700

    add a test with expected fail

--- notify-by-tag-py/test_send_follow_up_email.py
+++ notify-by-tag-py/test_send_follow_up_email.py
@@ -11,5 +11,32 @@ class TestSendFollwUpEmail(unittest.TestCase):
       ),
       ['hello@example.com', 'worl.d@example.local'])
 
+  def test_can_parse_nbtdata(self):
+    """parse each line of nbt_data into a python data structure
+    """
+
+    nbtdata_line_0 = '{"subject":"server under maintenance","author":"Sample Name <sample@example.com>","recipients":"Chalo Huan <chalohuan@example.com>, Cart <cart@example.com>"}'
+    nbtdata_line_1 = '{"subject":"status report - 20170614","author":"Nada <nada@example.com>","recipients":"status-report@example.com, Kwanjai A <kwanjai@example.com>, \\"Chalo D.\\" <chalod@example.com>"}'
+    nbtdata_line_2 = '{"subject":"Aug 4 - Aug 7, 30 people","author":"Boy Sawd <boy@example.org>","recipients":"Rui Nag <rui.nag@example.com>"}'
+
+    nbtdata = send_follow_up_email.parsenbtdata(nbtdata_line_0)
+
+    self.assertEqual(nbtdata['subject'], 'server under maintenance')
+    self.assertEqual(nbtdata['author'],  'Sample Name <sample@example.com>')
+    self.assertEqual(nbtdata['recipients'], 'Chalo Huan <chalohuan@example.com>, Cart <cart@example.com>')
+
+    nbtdata = send_follow_up_email.parsenbtdata(nbtdata_line_1)
+
+    self.assertEqual(nbtdata['subject'], 'status report - 20170614')
+    self.assertEqual(nbtdata['author'],  'Nada <nada@example.com>')
+    self.assertEqual(nbtdata['recipients'], 'status-report@example.com, Kwanjai A <kwanjai@example.com>, \\"Chalo D.\\" <chalod@example.com>"}')
+
+    nbtdata = send_follow_up_email.parsenbtdata(nbtdata_line_2)
+
+    self.assertEqual(nbtdata['subject'], 'Aug 4 - Aug 7, 30 people')
+    self.assertEqual(nbtdata['author'],  'Boy Sawd <boy@example.org>')
+    self.assertEqual(nbtdata['recipients'], 'Rui Nag <rui.nag@example.com>')
+
+
 if __name__ == '__main__':
   unittest.main()
