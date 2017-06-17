b69b16f3a12c23aa4f6a8ec114315dc737d12be9
dragontortoise <dragontortoise888@gmail.com>
2017-06-18 02:15:07 +0700

    This helper function prevents us from having to write duplicate code to build the sample raw contents inside nbt-data every time we want to test something.

--- notify-by-tag-py/test_send_follow_up_email.py
+++ notify-by-tag-py/test_send_follow_up_email.py
@@ -6,6 +6,25 @@ import send_follow_up_email
 
 class TestSendFollwUpEmail(unittest.TestCase):
 
+  def get_sample_raw_nbtdata_str(self) -> str:
+    """returns sample raw string in nbt-data file
+    """
+
+    raw = '{"subject":"server under maintenance",' + \
+      '"author":"Sample Name <sample@example.com>",' + \
+      '"recipients":"Chalo Huan <chalohuan@example.com>, ' + \
+      'Cart <cart@example.com>"}\n' + \
+      '{"subject":"status report - 20170614",' + \
+      '"author":"Nada <nada@example.com>",' + \
+      '"recipients":"status-report@example.com, ' + \
+      'Kwanjai A <kwanjai@example.com>, ' + \
+      '\\"Chalo D.\\" <chalod@example.com>"}\n' + \
+      '{"subject":"Aug 4 - Aug 7, 30 people",' + \
+      '"author":"Boy Sawd <boy@example.org>",' + \
+      '"recipients":"Rui Nag <rui.nag@example.com>"}'
+
+    return raw
+
   def test_grep_emails(self):
     self.assertEqual(send_follow_up_email.grep_emails(
         'How are you? hello@example.com hello \n<worl.d@example.local>'
@@ -16,20 +35,9 @@ class TestSendFollwUpEmail(unittest.TestCase):
     """parse each line of nbt_data into a python data structure
     """
 
-    nbtdata_line_0 = '{"subject":"server under maintenance",' + \
-      '"author":"Sample Name <sample@example.com>",' + \
-      '"recipients":"Chalo Huan <chalohuan@example.com>, ' + \
-      'Cart <cart@example.com>"}'
-    nbtdata_line_1 = '{"subject":"status report - 20170614",' + \
-      '"author":"Nada <nada@example.com>",' + \
-      '"recipients":"status-report@example.com, ' + \
-      'Kwanjai A <kwanjai@example.com>, ' + \
-      '\\"Chalo D.\\" <chalod@example.com>"}'
-    nbtdata_line_2 = '{"subject":"Aug 4 - Aug 7, 30 people",' + \
-      '"author":"Boy Sawd <boy@example.org>",' + \
-      '"recipients":"Rui Nag <rui.nag@example.com>"}'
+    nbtdata_lines = self.get_sample_raw_nbtdata_str().split('\n')
 
-    nbtdata = send_follow_up_email.parsenbtdata(nbtdata_line_0)
+    nbtdata = send_follow_up_email.parsenbtdata(nbtdata_lines[0])
 
     self.assertEqual(nbtdata['subject'], 'server under maintenance')
     self.assertEqual(nbtdata['author'],
@@ -37,7 +45,7 @@ class TestSendFollwUpEmail(unittest.TestCase):
     self.assertEqual(nbtdata['recipients'],
       'Chalo Huan <chalohuan@example.com>, Cart <cart@example.com>')
 
-    nbtdata = send_follow_up_email.parsenbtdata(nbtdata_line_1)
+    nbtdata = send_follow_up_email.parsenbtdata(nbtdata_lines[1])
 
     self.assertEqual(nbtdata['subject'], 'status report - 20170614')
     self.assertEqual(nbtdata['author'],  'Nada <nada@example.com>')
@@ -45,7 +53,7 @@ class TestSendFollwUpEmail(unittest.TestCase):
       'status-report@example.com, Kwanjai A <kwanjai@example.com>, ' + \
       '"Chalo D." <chalod@example.com>')
 
-    nbtdata = send_follow_up_email.parsenbtdata(nbtdata_line_2)
+    nbtdata = send_follow_up_email.parsenbtdata(nbtdata_lines[2])
 
     self.assertEqual(nbtdata['subject'], 'Aug 4 - Aug 7, 30 people')
     self.assertEqual(nbtdata['author'],  'Boy Sawd <boy@example.org>')
