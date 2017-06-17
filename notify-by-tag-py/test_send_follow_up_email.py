038aafc5f65dbae9498c704fee3fcc82c7864190
dragontortoise <dragontortoise888@gmail.com>
2017-06-18 01:49:07 +0700

    beautify code

--- notify-by-tag-py/test_send_follow_up_email.py
+++ notify-by-tag-py/test_send_follow_up_email.py
@@ -5,6 +5,7 @@ import unittest
 import send_follow_up_email
 
 class TestSendFollwUpEmail(unittest.TestCase):
+
   def test_grep_emails(self):
     self.assertEqual(send_follow_up_email.grep_emails(
         'How are you? hello@example.com hello \n<worl.d@example.local>'
